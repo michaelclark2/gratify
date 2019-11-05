530d2a598863eb38c5ccffca94b63d127c3be34d
michael <michael.clark1992@gmail.com>
2019-11-04 21:03:23 -0600

    added models with relationships

--- gratitude_backend/app/models.py
+++ gratitude_backend/app/models.py
@@ -1,3 +1,34 @@
 from app import db
 
 """ Models go here """
+
+prompt_entries = db.Table('prompt_entries', db.metadata,
+  db.Column('entry_id', db.Integer, db.ForeignKey('entry.id')),
+  db.Column('prompt_id', db.Integer, db.ForeignKey('prompt.id')))
+
+class Entry(db.Model):
+  __tablename__ = 'entry'
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.String(128), index=True)
+  prompt = db.relationship('Prompt', secondary=prompt_entries, backref='prompt')
+  response = db.Column(db.String(5000))
+
+  def __repr__(self):
+    return '<Entry {}>'.format(self.id)
+
+class Prompt(db.Model):
+  __tablename__ = 'prompt'
+  id = db.Column(db.Integer, primary_key=True)
+  text = db.Column(db.String(500))
+
+  def __repr__(self):
+    return '<Prompt {}>'.format(self.id)
+
+class Gratitude(db.Model):
+  __tablename__ = 'gratitude'
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.String(128), index=True)
+  response = db.Column(db.String(1000))
+
+  def __repr__(self):
+    return '<Gratitude {}>'.format(self.id)
--- gratitude_backend/app/routes.py
+++ gratitude_backend/app/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from app.models import Entry, Prompt, Gratitude
 
 @app.route('/')
 @app.route('/index')
 def index():
   return 'ayelmao'
+
+@app.route('/initdb')
+def initdb():
+  """seed me"""
+  return 'it worked?'
