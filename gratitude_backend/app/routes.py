9a33be0d530fc2a2ec115ee7e0d193ba892a9a18
michael <michael.clark1992@gmail.com>
2020-01-11 16:22:01 -0600

    added user route and notfound exception handling

--- gratitude_backend/app/routes.py
+++ gratitude_backend/app/routes.py
@@ -1,16 +1,42 @@
-from flask import json
-from app import app
+from flask import request, json, jsonify
+from app import app, db
 from app.models import Entry, Prompt, Gratitude, User
 from app.schemas import UserSchema, EntrySchema, PromptSchema, GratitudeSchema
+
+class NotFound(Exception):
+  status_code = 404
+  def __init__(self, message, status_code=None, payload=None):
+    Exception.__init__(self)
+    self.message = message
+    if status_code is not None:
+      self.status_code = status_code
+    self.payload = payload
+
+  def to_dict(self):
+    rv = dict(self.payload or ())
+    rv['message'] = self.message
+    return rv
+
+@app.errorhandler(NotFound)
+def handle_not_found(error):
+  response = jsonify(error.to_dict())
+  response.status_code = error.status_code
+  return response
 
 @app.route('/')
 @app.route('/index')
 def index():
   return 'ayelmao'
 
-@app.route('/users', methods=['GET'])
-def users():
+@app.route('/api/me', methods=['GET'])
+def get_user_by_firebase_id():
   user_schema = UserSchema()
-  users = User.query.all()
 
-  return user_schema.dumps(users, many=True)
+  fb_id = json.loads(request.data)['firebase_id']
+  user = db.session.query(User).filter(User.firebase_id == fb_id).first()
+
+  if user:
+    return user_schema.dumps(user)
+  else:
+    raise NotFound("Could not find user with firebase_id of '{}'".format(fb_id))
+
